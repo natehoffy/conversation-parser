84321102e38abd0a6d458f1e16b7405db363551d
Nate Hoffelmeyer <nhoffelmeyer@gmail.com>
2021-02-20 10:52:04 -0700

    fixed convo id abstraction

--- getMessages.js
+++ getMessages.js
@@ -1,2 +1,2 @@
 const axios = require('axios')
-return (await axios.get('https://driftapi.com/conversations/3024587240/messages?access_token=DRIFT_AUTH')).data.messages;
+return (await axios.get('https://driftapi.com/conversations/{conversationId}/messages?access_token=DRIFT_AUTH')).data.messages;
