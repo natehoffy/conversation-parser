c228a7e1abff66b05837e6371d9e7730c30dd17f
Joshua Perk <joshrsperk@gmail.com>
2021-03-31 22:12:30 -0400

    Updated using promises (#4)
    Approved

--- app.js
+++ app.js
@@ -17,28 +17,49 @@ app.listen(PORT, () => console.log(`Testing app listening on port ${PORT}!`))
 ngrok.connect(PORT).then(url => console.log(`External Forwarding URL is: ${url}/api`))
 
 // POST request to our /api endpoint
-app.post('/api', (req, res) => {
-
-    // pull out the JSON message
-    const conversationId = req.body.data.data.conversationId
-
-
-    // log the response, use the util function to parse all the way into the object
-    console.log(util.inspect(conversationId, {showHidden: false, depth: null}))
-    console.log(getConversation(conversationId))
-
-    // end the connection with a positive response
-    res.status(200)
-    return res.end()
+// This needs to have the ASYNC keyword because inside this function is a call to another async function meaning we need to wait for it
+app.post('/api', async (req, res) => {
+    // Put the whole function in a try/catch loop
+    // That way if something goes wrong, we can understand it
+    // And not throw a fatal error, crashing the whole service
+    try {
+        // Get the convoId, if for some reason that element didn't exist in the request, it would throw an error
+        const conversationId = req.body.data.data.conversationId
+        // Use AWAIT to tell this function to hang here until getConversation resolves/rejects a promise to fulfill the conversation data
+        const conversation = await getConversation(conversationId)
+        // Check if an error message is included in the response and if not...
+        if (!conversation.error) {
+            // Log the success
+            console.log('It worked!', conversation.data)
+            // Also resolve the request successfully
+            return res.status(200).json(conversation.data);
+        } else {
+            // Otherwise, log the failure
+            console.log('Something went wrong:', conversation.error)
+            // And hangup the request with an error message
+            return res.status(500).json({error: conversation.error});
+        }
+    } catch (e) {
+        // Catch the generic error and log it
+        console.log('Something went wrong.')
+        // Also end the request
+        return res.status(500).json({error: e});
+    }
 })
 
 // HELPER functions
 
 // look up meta data about this conversation
 const getConversation = (conversationId) => {
-    console.log("Retrieving conversation")
     return request.get(CONVERSATION_API_BASE + `/${conversationId}`)
         .set('Content-type', 'application/json')
-        .set(`Authorization`, `bearer ${TOKEN}`)
-        .catch(err => console.log(err))
+        .set('Authorization', `Bearer ${TOKEN}`)
+        .then(res => {
+            return res.body
+        })
+        .catch(err => {
+            return {
+                error: err.message
+            }
+        });
 }
